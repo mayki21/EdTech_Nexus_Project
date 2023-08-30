e2853a8ae2dc65c0847593ed17b1a7e4ee01d09a
mayank <mayank.khobragade9891@gmail.com>
2023-08-30 18:26:17 +0530

    frontend_Added

--- backend/course/urls.py
+++ backend/course/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('create',views.CreatCourse,name='create'),
-    path('get',views.Getcourse,name='get')
+    path('create',views.CreateCourse,name='create'),
+    path('get',views.GetCourse,name='get')
 
 ]
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
