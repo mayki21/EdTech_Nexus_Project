f5d35aa31703fd23c2033822cf66cc589af010a3
mayank <mayank.khobragade9891@gmail.com>
2023-09-01 22:46:48 +0530

    update backend assignment

--- backend/assignment/urls.py
+++ backend/assignment/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('create/<int:courseid>',views.CreateAssignment,name="create"),
     path('get',views.SeeAssignment,name='get'),
-    path('see/<int:assid>',views.ParticularAssignment,name='see')
+    path('see/<int:assid>',views.ParticularAssignment,name='see'),
+    path('partcourse/<int:courseid>',views.ParticularCourse,name='partcourse')
 ]
