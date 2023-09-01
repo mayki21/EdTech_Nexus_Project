0fcc983ad5a0ed24ac0fc380a4f514d9b41b7f86
mayank <mayank.khobragade9891@gmail.com>
2023-09-01 23:04:48 +0530

    app routing updated

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InstructorComponent } from './pages/instructor/instructor.component';
+import { InstructorassignComponent } from './pages/instructorassign/instructorassign.component';
 // import { HomeComponent } from './pages/home/home.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -15,7 +17,9 @@ const routes: Routes = [
   {path:'register',component:RegisterComponent},
   {path:'course',component:StudentenrollComponent},
   {path:'assignment',component:AssignmentComponent},
-  {path:'particular',component:ParticularComponent}
+  {path:'particular',component:ParticularComponent},
+  {path:'instructor',component:InstructorComponent},
+  {path:'instructorassign',component:InstructorassignComponent}
 
 ];
 
